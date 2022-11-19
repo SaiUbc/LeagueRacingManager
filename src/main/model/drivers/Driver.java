0edc62d81c4305afcd87daab23d5a50d2330e571
SaiUbc <99237847+SaiUbc@users.noreply.github.com>
2022-11-19 03:14:26 -0800

    implemented proper interface for driver roles

--- src/main/model/drivers/Driver.java
+++ src/main/model/drivers/Driver.java
@@ -1,5 +1,29 @@
 package src.main.model.drivers;
 
+import src.main.model.Team;
+
 public interface Driver {
-    
+
+    //EFFECTS: returns the drivers name future implementation could include discord_id
+    String getDriverName();
+
+    //EFFECTS: returns the drivers racing number
+    int getDriverNumber();
+
+    //EFFECTS: returns the drivers Team
+    Team getDriverTeam();
+
+    //EFFECTS: returns the driver's Full time status
+    boolean getDriverFullTimeStatus();
+
+
+    //setters
+    void setDriverName(String driverName);
+
+    void setDriverNumber(int driverNumber);
+
+    void setDriverTeam(Team driverTeam);
+
+    void setDriverFullTimeStatus(boolean status);
+
 }
