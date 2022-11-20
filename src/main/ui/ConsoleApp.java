b29d2cbae58b16916e112eb984793f38fcee8495
SaiUbc <99237847+SaiUbc@users.noreply.github.com>
2022-11-20 01:47:00 -0800

    implementation of console app which taken name inp

--- src/main/ui/ConsoleApp.java
+++ src/main/ui/ConsoleApp.java
@@ -1,5 +1,12 @@
 package src.main.ui;
 
+import java.util.Scanner;
+
 public class ConsoleApp {
-    
+    public ConsoleApp() {
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        name = sc.nextLine();
+    }
 }
