4bec7c5d9fb19ac846a4f82a5f94042a2941983c
SaiUbc <99237847+SaiUbc@users.noreply.github.com>
2022-11-25 03:17:04 -0800

    initialization of name, distance and laps for a race

--- src/main/model/calendar/raceWeekend/race/race.java
+++ src/main/model/calendar/raceWeekend/race/race.java
@@ -2,4 +2,33 @@ package src.main.model.calendar.raceWeekend.race;
 
 public class race {
     //TODO: actual race distance, name, country.
+    private String name;
+    private RaceDistance distance;
+    private int laps;
+
+
+    //getters
+    public int getLaps() {
+        return laps;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public RaceDistance getDistance() {
+        return distance;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDistance(RaceDistance distance) {
+        this.distance = distance;
+    }
+
+    public void setLaps(int laps) {
+        this.laps = laps;
+    }
 }
